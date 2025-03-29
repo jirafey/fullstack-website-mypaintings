0362d1570eaf5c017b5421fc26bc331657ca407f
Grzegorz Kmita <kg53848@home2.zut.edu.pl>
2025-03-29 22:15:36 +0100

    a

--- pull_repo.sh
+++ pull_repo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd /home/students/k/kg/kg53848/public_html/git/main && git pull origin main
+# Navigate to the directory containing the repository
+cd /home/students/k/kg/kg53848/public_html/git/IPZ-mypaintings || exit 1
+
+# Run the pull_repo.sh script
+./pull_repo.sh
 
 
 
