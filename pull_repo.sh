bba2c40fff0a8a59987a05c88ded68b0391200db
Grzegorz Kmita <kg53848@home2.zut.edu.pl>
2025-03-29 22:12:53 +0100

    a

--- pull_repo.sh
+++ pull_repo.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-while true; do
-    cd /home/students/k/kg/kg53848/public_html/git/main && git pull origin main
-    sleep 1
-done
+cd /home/students/k/kg/kg53848/public_html/git/main && git pull origin main
 
 
 
