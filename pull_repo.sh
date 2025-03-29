4c0e93e91433e4906dce817890f82ce0a172360c
Grzegorz Kmita <kg53848@home2.zut.edu.pl>
2025-03-29 21:47:20 +0100

    a

--- pull_repo.sh
+++ pull_repo.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 while true; do
-    cd /home/students/k/kg/kg53848/public_html/git/main && git pull origin main
+    cd /home/students/k/kg/kg53848/public_html/git/main || exit 1
+    
+    # Pull the latest changes from the remote repository and force update
+    git fetch origin main
+    git reset --hard origin/main  # This resets your local branch to match the remote
+
+    # Pause for 1 second before the next check
     sleep 1
 done
+
 
