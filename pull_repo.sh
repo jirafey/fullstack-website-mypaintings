3693675ed80a6825f790287752c1f7b718959b44
jirafey <jirafey>
2025-03-30 16:53:20 +0200

    Add preivous changes

--- pull_repo.sh
+++ pull_repo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 # Navigate to the directory containing the repository
 cd /home/students/k/kg/kg53848/public_html/git/IPZ-mypaintings || exit 1
 
@@ -7,4 +8,10 @@ cd /home/students/k/kg/kg53848/public_html/git/IPZ-mypaintings || exit 1
 ./pull_repo.sh
 
 
+=======
+while true; do
+    cd /home/students/k/kg/kg53848/public_html/git/grzegorz && git pull origin grzegorz
+    sleep 1
+done
+>>>>>>> d8edc13 (CI/CD test)
 
